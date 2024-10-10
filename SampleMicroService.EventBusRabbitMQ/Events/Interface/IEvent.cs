2f6ecd5eba182afd386af89300be677ee2f5ae55
Osman Fitrat <osman.karamizrak@gmail.com>
2024-10-11 02:15:37 +0300

    RabbiMQ and Event Bus implemantation

---
+++ SampleMicroService.EventBusRabbitMQ/Events/Interface/IEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMicroService.EventBusRabbitMQ.Events.Interface
+{
+    public abstract class  IEvent
+    {
+        protected IEvent()
+        {
+            RequestId = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public Guid RequestId { get; private init; }
+        public DateTime CreationDate { get; private init; }
+
+    }
+}
